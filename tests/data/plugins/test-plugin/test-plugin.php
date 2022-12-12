52fd9bd8378cef93a7d95c9e9bf65c0e0d127bc9
Andrew Matthews <andrew.matthews@wpengine.com>
2022-12-12 10:37:44 -0500

    Add e2e test using GitHub Actions (#8)
    * Add e2e test using GitHub Actions
    
    * Fix path to Dockerfile
    
    * Run initial deploy
    
    * Restore full test
    
    * Add message for debugging post-deploy script
    
    * Try quoting flags so backup isn't skipped
    
    * Fix missing multiplex connection in rsync
    
    * Use underscore in vendor name for consistency
    
    * Enable all workflow triggers
    
    * Run validate_result job even on failed deploy

---
+++ tests/data/plugins/test-plugin/test-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Deploy WordPress to WP Engine - e2e Test
+ * Plugin URI: https://github.com/wpengine/site-deploy
+ * Description: Sample code to test the wpengine/site-deploy image.
+ * Version: 0.0.1
+ */
+ 
+ add_action('init', 'register_my_cpt');
+
+ function register_my_cpt() {
+    register_post_type('my-cpt', array(
+        'public' => true
+    ));
+ }
