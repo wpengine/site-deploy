06c357569c501ca8bc0341bebbea805a2aa2338d
Christine Seeman <christine.seeman@wpengine.com>
2024-10-10 16:22:11 -0500

    fixing tests

--- tests/test-make-relative-remote/test.sh
+++ tests/test-make-relative-remote/test.sh
@@ -19,45 +19,34 @@ setup() {
 # Test resulting directory structure from calling make_relative_remote
 # Expected output is the directory structure after moving the contents of SRC_PATH to REMOTE_PATH
 # e.g. make_relative_remote "/home/user/website" "/var/www/html" should return "/var/www/html/website"
-# 1st argument: SRC_PATH, 2nd argument: REMOTE_PATH, 3rd argument: EXPECTED_REMOTE_PATH
+# 1st argument: SRC_PATH, 2nd argument: REMOTE_PATH
+# How can I make sure the make test is not caching the docker and creating it fresh
 test_make_relative_remote() {
   setup "$1"
   SRC_PATH=$2
   REMOTE_PATH=$3
-  EXPECTED_REMOTE_PATH=$4
 
-  echo -e "${GREEN}REMOTE_PATH='$REMOTE_PATH' SRC_PATH='$SRC_PATH' EXPECTED_REMOTE_PATH='$EXPECTED_REMOTE_PATH'" 
+  echo -e "${GREEN}REMOTE_PATH='$REMOTE_PATH' SRC_PATH='$SRC_PATH'${NC}" 
   make_relative_remote
 
-  if [[ "$REMOTE_PATH" != "$EXPECTED_REMOTE_PATH" ]]; then
-      echo -e "${RED}Test failed for SRC_PATH='$SRC_PATH' and REMOTE_PATH='$REMOTE_PATH': expected '$EXPECTED_REMOTE_PATH', got '$REMOTE_PATH'.${NC}"
-      return
+  # Only compare the expected directory structure if REMOTE_PATH is not empty
+  if [[ -n "$REMOTE_PATH" && "$REMOTE_PATH" != "$SRC_PATH" ]]; then
+      # Verify that REMOTE_PATH and its folders exist in /workspace
+    echo "Verifying that REMOTE_PATH and its folders exist in /workspace"
+    if [ -d "/workspace/$REMOTE_PATH" ]; then
+        echo -e "${GREEN}REMOTE_PATH exists in /workspace"
+    else
+        echo -e "${RED}Verification failed: REMOTE_PATH does not exist in /workspace.${NC}"
+    fi
+  else
+    echo -e "${YELLOW}REMOTE_PATH is empty or equal to SRC_PATH, skipping comparison.${NC}"
   fi
-  # Remote path does not change, we need to be checking that the workspace directory structure matches 
-  # Make sure that the remote path exists in the workspace directory structure
 }
 
 # Test cases, make remote directory relative to to the tests directory
-test_make_relative_remote "1" "." "" ""
-test_make_relative_remote "2" "./wp-content" "./wp-content" "./wp-content"
-test_make_relative_remote "3" "." "wp-content/" "wp-content/"
-#test_make_relative_remote "./test_dir/remote/wp-content" ".test_dir/local/wp-content" "./test_dir/remote/wp-content"
-
-# Individual theme, need to make sure resulting folder structure matches expected excludes
-# REMOTE_PATH=/wp-content/themes/beautiful-pro
-# SRC_PATH=.
-# EXPECTED_REMOTE_PATH=create wp-content/themes/beautiful-pro, copy all contents of beautiful-pro into the folder.
-
-# Use a different SRC_PATH besides .
-# .env is at the root of the wordpress site, then the 
-# SRC_PATH= is very flexible, this is relative to your own repo
-# SRC_PATH=my-awesome-plugins
-# REMOTE_PATH=wp-content/plugins
-
-# Check that this is deploying plugin folders into the plugins directory
-#SRC_PATH=my-awesome-plugins/the-newness
-# REMOTE_PATH=wp-content/plugins/
-
-# Check that just the akismet plugin is being deployed to the plugins directory
-#SRC_PATH=plugins/akismet
-#REMOTE_PATH=wp-content/plugins/
+test_make_relative_remote "1" "." ""
+test_make_relative_remote "2" "./wp-content" "./wp-content"
+test_make_relative_remote "3" "." "wp-content/"
+test_make_relative_remote "4" "." "wp-content/themes/beautiful-pro" 
+test_make_relative_remote "5" "my-awesome-plugins" "wp-content/plugins" 
+test_make_relative_remote "6" "my-awesome-plugins/blues-brothers" "wp-content/plugins" 
