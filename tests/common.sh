3351cbde8639e4b6572b5e9d7c9370f8c37096b7
Michael Day <michael.day@wpengine.com>
2024-12-03 22:56:52 -0600

    [CICD-761] Explicitly export color globals
    Resolves a warning about unused variables in the common file.

--- tests/common.sh
+++ tests/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+export GREEN='\033[0;32m'
+export RED='\033[0;31m'
+export BLUE='\033[0;34m'
+export NC='\033[0m' # No Color
