6e0f064a68876e0cb61f35b1cd8688069b6ab0b3
Michael Day <michael.day@wpengine.com>
2024-12-18 12:23:51 -0600

    [CICD-761] Move to a simpler approach for unit tests

--- Makefile
+++ Makefile
@@ -22,24 +22,6 @@ version: build
 	docker image tag $(IMAGE) $(IMAGE_NAME):v$(MAJOR_VERSION).$(MINOR_VERSION) && \
 	docker image tag $(IMAGE) $(IMAGE_NAME):v$(MAJOR_VERSION).$(MINOR_VERSION).$(PATCH_VERSION)
 
-test:
+test-unit:
 	./tests/test_functions.sh
-#	./tests/test_exclude_from.sh
-	@make test-excludes
-
-# Test that rsync is properly parsing dynamic file/path exclusions
-# Running with an image to emulate the way in which generate_path_excludes.sh will actually be used
-test-excludes:
-	@echo "🚧 Building container for testing dynamic file/path exclusions with rsync... 🚧"
-	@docker build -f tests/docker/Dockerfile_test-path-excludes -t test-path-excludes .
-	@echo "🚧 Running tests... 🚧"
-	@docker run --rm -e REPO_PATH=$(PWD) -v $(PWD)/tests/fixtures/src:/site --workdir /site test-path-excludes
-	@docker run --rm -e REPO_PATH=$(PWD) -v $(PWD)/tests/fixtures/src:/site --workdir /site/wp-content test-path-excludes
-	@docker run --rm -e REPO_PATH=$(PWD) -v $(PWD)/tests/fixtures/src:/site --workdir /site/wp-content/plugins test-path-excludes
-	@docker run --rm -e REPO_PATH=$(PWD) -v $(PWD)/tests/fixtures/src/wp-content:/site --workdir /site test-path-excludes
-	@docker run --rm -e REPO_PATH=$(PWD) -v $(PWD)/tests/fixtures/src/wp-content:/site --workdir /site/plugins test-path-excludes
-	@docker run --rm -e REPO_PATH=$(PWD) -v $(PWD)/tests/fixtures/src/wp-content/plugins:/site --workdir /site test-path-excludes
-	#@docker run --rm -v $(PWD)/tests/fixtures/src:/site --workdir /site/wp-content/mu-plugins test-path-excludes #MU-PLUGINS
-	#@docker run --rm -v $(PWD)/tests/fixtures/src/wp-content:/site --workdir /site/mu-plugins test-path-excludes #MU-PLUGINS
-	#@docker run --rm -v $(PWD)/tests/fixtures/src/wp-content/mu-plugins:/site --workdir /site test-path-excludes #MU-PLUGINS
-	@echo "🚀 All tests passed for dynamic file/path exclusions with rsync 🚀"
+	./tests/test_generate_path_excludes.sh
--- utils/generate_path_excludes.sh
+++ utils/generate_path_excludes.sh
@@ -3,6 +3,34 @@
 set -e
 shopt -s extglob
 
+# Dynamic Excludes
+#
+# This script generates two lists of files to exclude; one for the source, and one for the remote.
+#
+# Notes about excluded files:
+#
+#   Version Control
+#   NOTE:
+#     WP Engine does not support server side versioning so hosting any version control
+#     on the server would not be advantageous.
+#
+#   WordPress specific files
+#   NOTE:
+#     These files are excluded from the deploy so as to prevent unwanted errors from occurring,
+#     such as accidentally deploying a local version of wp-config.php or accidentally deleting
+#     wp-content/uploads/ if a --delete flag is passed while deploying root. Most paths here
+#     are ignored in the WPE sample .gitignore per best practice.
+#
+#   WP Engine specific files
+#   NOTE:
+#     These files are specific to running a WordPress site at WP Engine and would
+#     likely result in a broken production site if modified in production (in
+#     fact, permissions would prevent modification for many of these files). While
+#     some of these files (such as those in /_wpeprivate) would be extremely large
+#     and completely useless in the context of local WordPress development, others
+#     (such as some of the WP Engine managed plugins) might be useful in rare
+#     circumstances to have as a reference for debugging purposes.
+
 # Get the directory of the current script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -10,152 +38,138 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/print_mount_paths.sh"
 
 # Determine the source paths to exclude from the deployment
-#
-# The function returns three values: the absolute base path for prefixing excluded files,
-# the absolute mu-plugins path, and a debug string indicating which switch case was matched.
 determine_source_exclude_paths() {
   local base_path
   local mu_dir_path
-  local debug
 
-  read -r mounted_source_path base_mount_path <<< "$(print_mount_paths "/wp-content")"
+  read -r mounted_source_path _ <<< "$(print_mount_paths "wp-content")"
 
-  if [[ -n "${SRC_PATH}" && "${SRC_PATH}" != '.' ]] ||
-  [[ "$(pwd)" == @(*wp-content|*mu-plugins) ]] ||
-  [[ "$(pwd)" == "$base_mount_path" && -n "${mounted_source_path}" ]]; then
-    if [[ -n "${SRC_PATH}" && "${SRC_PATH}" != '.' ]]; then
-      case "${SRC_PATH}" in
-        wp-content )
-          base_path="/wp-content/"
-          mu_dir_path="/wp-content/mu-plugins/"
-          debug="SRC_PATH switch: matched 'wp-content'"
-          ;;
-        wp-content/ | *mu-plugins )
+  if [[ -n "${SRC_PATH}" && "${SRC_PATH}" != '.' ]]; then
+    case "${SRC_PATH}" in
+      wp-content )
+        base_path="/wp-content/"
+        mu_dir_path="/wp-content/mu-plugins/"
+        ;;
+      wp-content/ | *mu-plugins )
+        base_path="/"
+        mu_dir_path="/mu-plugins/"
+        ;;
+      *mu-plugins/ )
+        mu_dir_path="/"
+        ;;
+    esac
+  elif [[ "$(pwd)" != *wp-content* && "${mounted_source_path}" != *wp-content* ]]; then
+    base_path="/wp-content/"
+    mu_dir_path="/wp-content/mu-plugins/"
+  else
+    # Iterate over the possible paths and break when a match is found
+    # !!! Ordering is important for the switch cases !!!
+    for value in "$(pwd)" "$mounted_source_path"; do
+      case "$value" in
+        *wp-content )
           base_path="/"
           mu_dir_path="/mu-plugins/"
-          debug="SRC_PATH switch: 'wp-content/ | *mu-plugins'"
+          break
           ;;
-        *mu-plugins/ )
+        *mu-plugins )
           mu_dir_path="/"
-          debug="SRC_PATH switch: '*mu-plugins/'"
+          break
           ;;
       esac
-    else
-      # Iterate over the possible paths and break when a match is found
-      # !!! Ordering is important for the switch cases !!!
-      for value in "$(pwd)" "$mounted_source_path"; do
-        case "$value" in
-          *wp-content )
-            base_path="/"
-            mu_dir_path="/mu-plugins/"
-            debug="Matched: *wp-content"
-            break
-            ;;
-          *mu-plugins )
-            mu_dir_path="/"
-            debug="Matched: *wp-content/mu-plugins"
-            break
-            ;;
-        esac
-      done
-    fi
+    done
   fi
 
-  printf "%s\n%s\n%s\n" "$base_path" "$mu_dir_path" "$debug"
+  printf "%s\n%s\n" "$base_path" "$mu_dir_path"
 }
 
 determine_remote_exclude_paths() {
   local base_path
   local mu_dir_path
-  local debug="default"
 
-  if [[ -n "${REMOTE_PATH}" ]]; then
+  if [[ -z "${REMOTE_PATH}" ]]; then
+    base_path="/wp-content/"
+    mu_dir_path="/wp-content/mu-plugins/"
+  else
     case "$REMOTE_PATH" in
       wp-content?(/) )
-        base_path=""
-        mu_dir_path="mu-plugins/"
-        debug="REMOTE_PATH switch: 'wp-content?(/)'"
+        base_path="/"
+        mu_dir_path="/mu-plugins/"
         ;;
       wp-content/mu-plugins?(/) )
-        mu_dir_path=""
-        debug="REMOTE_PATH switch: 'wp-content/mu-plugins'"
+        mu_dir_path="/"
         ;;
     esac
   fi
 
-  printf "%s\n%s\n%s\n" "$base_path" "$mu_dir_path" "$debug"
+  printf "%s\n%s\n" "$base_path" "$mu_dir_path"
 }
 
-# Generate a list of files to exclude from the deployment
-#
-# The list is output to stdout, which can be used as an argument to --exclude-from.
+# Generate the dynamic list of files to exclude from the deployment
 print_dynamic_excludes() {
   local func=$1
+  local delimiter=$2
+  local output=""
+
   {
     IFS= read -r base_path
     IFS= read -r mu_dir_path
-    IFS= read -r debug
   } <<< "$($func)"
 
-  if [[ -n "$base_path" && -n "$mu_dir_path" ]]; then
-    cat << EOF
-# WordPress specific files (dynamic paths)
-${base_path}uploads/
-${base_path}blogs.dir/
-${base_path}upgrade/*
-${base_path}backup-db/*
-${base_path}advanced-cache.php
-${base_path}wp-cache-config.php
-${base_path}cache/*
-${base_path}cache/supercache/*
+  local wp_content_exclude_paths=(
+    # WordPress specific files and directories
+    uploads/
+    blogs.dir/
+    upgrade/*
+    backup-db/*
+    advanced-cache.php
+    wp-cache-config.php
+    cache/*
+    cache/supercache/*
+    # WP Engine specific files and directories
+    object-cache.php
+    drop-ins/
+    drop-ins/wp-cache-memcached*
+    mysql.sql
+  )
 
-# WP Engine specific files (dynamic paths)
-${base_path}object-cache.php
-${base_path}drop-ins/
-${base_path}drop-ins/wp-cache-memcached*
-${base_path}mysql.sql
-${mu_dir_path}mu-plugin.php
-${mu_dir_path}slt-force-strong-passwords.php
-${mu_dir_path}wpengine-security-auditor.php
-${mu_dir_path}stop-long-comments.php
-${mu_dir_path}force-strong-passwords*
-${mu_dir_path}wpengine-common*
-${mu_dir_path}wpe-wp-sign-on-plugin*
-${mu_dir_path}wpe-elasticpress-autosuggest-logger*
-${mu_dir_path}wpe-cache-plugin*
-${mu_dir_path}wp-cache-memcached*
+  local mu_plugin_exclude_paths=(
+    # WP Engine specific files and directories
+    mu-plugin.php
+    slt-force-strong-passwords.php
+    wpengine-security-auditor.php
+    stop-long-comments.php
+    force-strong-passwords*
+    wpengine-common*
+    wpe-wp-sign-on-plugin*
+    wpe-elasticpress-autosuggest-logger*
+    wpe-cache-plugin*
+    wp-cache-memcached*
+    # Local specific files
+    local-by-flywheel-live-link-helper.php
+  )
 
-# Local specific (dynamic paths)
-${mu_dir_path}local-by-flywheel-live-link-helper.php
+  if [[ -n "$base_path" ]]; then
+    output+="${delimiter}### Dynamic file and directory exclusions${delimiter}"
+    for path in "${wp_content_exclude_paths[@]}"; do
+      output+="${base_path}$path${delimiter}"
+    done
+  fi
 
-#################### Debug: ${debug}
-EOF
-  elif [[ -n "$mu_dir_path" ]]; then
-    cat << EOF
-# WP Engine specific files (dynamic paths)
-${mu_dir_path}mu-plugin.php
-${mu_dir_path}slt-force-strong-passwords.php
-${mu_dir_path}wpengine-security-auditor.php
-${mu_dir_path}stop-long-comments.php
-${mu_dir_path}force-strong-passwords*
-${mu_dir_path}wpengine-common*
-${mu_dir_path}wpe-wp-sign-on-plugin*
-${mu_dir_path}wpe-elasticpress-autosuggest-logger*
-${mu_dir_path}wpe-cache-plugin*
-${mu_dir_path}wp-cache-memcached*
+  if [[ -n "$mu_dir_path" ]]; then
+    output+="${delimiter}### Dynamic mu-plugin file and directory exclusions${delimiter}"
+    for path in "${mu_plugin_exclude_paths[@]}"; do
+      output+="${mu_dir_path}$path${delimiter}"
+    done
+  fi
 
-# Local specific (dynamic paths)
-${mu_dir_path}local-by-flywheel-live-link-helper.php
+  echo -e "$output"
+}
 
-#################### Debug: ${debug}
-EOF
-  else
-    cat << EOF
+generate_source_exclude_from() {
+  local dynamic_excludes; dynamic_excludes=$(print_dynamic_excludes determine_source_exclude_paths "\n")
+
+  cat << EOF
 # Version Control
-# NOTE:
-#    WP Engine does not support server side versioning so hosting any version control
-#    on the server would not be advantageous.
-
 *~
 .git
 .github
@@ -168,60 +182,31 @@ EOF
 Thumbs.db
 
 # WordPress specific files
-# NOTE:
-#     These files are excluded from the deploy so as to prevent unwanted errors from occurring,
-#     such as accidentally deploying a local version of wp-config.php or accidentally deleting
-#     wp-content/uploads/ if a --delete flag is passed while deploying root. Most paths here
-#     are ignored in the WPE sample .gitignore per best practice.
 wp-config.php
-wp-content/uploads/
-wp-content/blogs.dir/
-wp-content/upgrade/*
-wp-content/backup-db/*
-wp-content/advanced-cache.php
-wp-content/wp-cache-config.php
-wp-content/cache/*
-wp-content/cache/supercache/*
 
-# WP Engine specific files
-# NOTE:
-#   These files are specific to running a WordPress site at WP Engine and would
-#   likely result in a broken production site if modified in production (in
-#   fact, permissions would prevent modification for many of these files). While
-#   some of these files (such as those in /_wpeprivate) would be extremely large
-#   and completely useless in the context of local WordPress development, others
-#   (such as some of the WP Engine managed plugins) might be useful in rare
-#   circumstances to have as a reference for debugging purposes.
+# WP Engine specific files and directories
 .smushit-status
 .gitattributes
 .wpe-devkit/
 .wpengine-conf/
 _wpeprivate
-wp-content/object-cache.php
-wp-content/drop-ins/
-wp-content/drop-ins/wp-cache-memcached*
-wp-content/mysql.sql
-wp-content/mu-plugins/mu-plugin.php
-wp-content/mu-plugins/slt-force-strong-passwords.php
-wp-content/mu-plugins/wpengine-security-auditor.php
-wp-content/mu-plugins/stop-long-comments.php
-wp-content/mu-plugins/force-strong-passwords*
-wp-content/mu-plugins/wpengine-common*
-wp-content/mu-plugins/wpe-wp-sign-on-plugin*
-wp-content/mu-plugins/wpe-elasticpress-autosuggest-logger*
-wp-content/mu-plugins/wpe-cache-plugin*
-wp-content/mu-plugins/wp-cache-memcached*
-
-# Local specific
-wp-content/local-by-flywheel-live-link-helper.php
+$(echo -e "$dynamic_excludes")
 EOF
-  fi
-}
-
-generate_source_exclude_from() {
-  print_dynamic_excludes determine_source_exclude_paths
 }
 
 generate_remote_excludes() {
-  print_dynamic_excludes determine_remote_exclude_paths | grep -Ev '^\s*(#|$)' | awk '{printf "--exclude='\''%s'\'' ", $0}' | sed 's/[[:space:]]*$//'
+  local dynamic_excludes; dynamic_excludes=$(print_dynamic_excludes determine_remote_exclude_paths "\n")
+
+  cat << EOF | grep -Ev '^\s*(#|$)|\s+#' | awk '{printf "--exclude='\''%s'\'' ", $0}' | sed 's/[[:space:]]*$//'
+# WordPress specific files
+wp-config.php
+
+# WP Engine specific files and directories
+.smushit-status
+.gitattributes
+.wpe-devkit/
+.wpengine-conf/
+_wpeprivate
+$(echo -e "$dynamic_excludes")
+EOF
 }
