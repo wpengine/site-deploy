cec397d1a0347be2dfbeef35034a018a14e2b69b
Michael Day <michael.day@wpengine.com>
2024-12-18 12:23:51 -0600

    [CICD-761] Move to a simpler approach for unit tests

--- tests/test_generate_path_excludes.sh
+++ tests/test_generate_path_excludes.sh
@@ -1,129 +1,193 @@
 #!/bin/bash
 
+set -e
+
 # Get the directory of the current script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source "${SCRIPT_DIR}/common.sh"
-source "${SCRIPT_DIR}/log_debug.sh"
-source "${SCRIPT_DIR}/print_mount_paths.sh"
-source "${SCRIPT_DIR}/generate_path_excludes.sh"
+source "${SCRIPT_DIR}/helpers/common.sh"
+source "${SCRIPT_DIR}/helpers/change_working_dir.sh"
+source "${SCRIPT_DIR}/../utils/generate_path_excludes.sh"
 
-test_rsync_with_excludes() {
-  export SRC_PATH=$1 REMOTE_PATH=$2
-  # shellcheck disable=SC2206
-  local FLAGS=($3)
+test_determine_source_exclude_paths() {
+  run_test_determine_source_exclude_paths() {
+    export SRC_PATH=$1
+    local expected_base_path=$2
+    local expected_mu_dir_path=$3
 
-  # Generate the exclusion lists before using the output with rsync
-  local source_exclude_from; source_exclude_from="$(generate_source_exclude_from)"
-  local remote_excludes; remote_excludes="$(generate_remote_excludes)"
+    {
+      IFS= read -r base_path || base_path=""
+      IFS= read -r mu_dir_path || mu_dir_path=""
+    } <<< "$(determine_source_exclude_paths)"
 
-  # Capture the output of the rsync command
-  output=$(rsync --dry-run -azvr --inplace --exclude='.*' "${FLAGS[@]}" \
-                 --exclude-from=<(echo "$source_exclude_from") \
-                 --rsync-path="rsync $remote_excludes" \
-                 "$SRC_PATH" "$REPO_PATH/tests/fixtures/remote/$REMOTE_PATH")
-
-  if [[ "$SRC_PATH" != *mu-plugins?(/) && "$(pwd)" != *mu-plugins ]]; then
-    # Fail if the output is missing the expected file
-    if ! echo "$output" | grep -q "test-plugin.php"; then
-      echo -e "${RED}Test failed for pwd='$(pwd)', SRC_PATH='$SRC_PATH', REMOTE_PATH='$REMOTE_PATH': test-plugin.php excluded${NC}"
-      echo -e "${BLUE}$(log_debug "Rsync output:" "$output")\n${NC}"
-      echo -e "${BLUE}$(log_debug "Source exclude from:" "$source_exclude_from")\n${NC}"
-      echo -e "${BLUE}$(log_debug "Remote excludes:" "$remote_excludes")\n${NC}"
+    if [[ "${base_path}" != "${expected_base_path}" || "${mu_dir_path}" != "${expected_mu_dir_path}" ]]; then
+      read -r mounted_source_path _ <<< "$(print_mount_paths "tests/fixtures/src")"
+      echo -e "${RED}Test failed for SRC_PATH='${SRC_PATH}': expected '$expected_base_path, $expected_mu_dir_path', got '$base_path, $mu_dir_path'.${NC}"
+      echo -e "${BLUE}INFO: mounted_source_path='${mounted_source_path}, pwd=$(pwd)'${NC}"
       exit 1
     fi
+  }
 
-    # Fail if the output is missing the nested plugin file that is excluded at a different path
-    if ! echo "$output" | grep -q "cache/object-cache"; then
-      echo -e "${RED}Test failed for pwd='$(pwd)', SRC_PATH='$SRC_PATH', REMOTE_PATH='$REMOTE_PATH': cache/object-cache.php excluded${NC}"
-      echo -e "${BLUE}$(log_debug "Rsync output:" "$output")\n${NC}"
-      echo -e "${BLUE}$(log_debug "Source exclude from:" "$source_exclude_from")\n${NC}"
-      echo -e "${BLUE}$(log_debug "Remote excludes:" "$remote_excludes")\n${NC}"
+  mount_from_root() {
+    echo -e "${BLUE}INFO: mounting from /site...${NC}"
+
+    # Mock the print_mount_paths function
+    print_mount_paths() {
+      # shellcheck disable=SC2317
+      printf "/path/to/site /site"
+    }
+
+    # Workdir -> src directory
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "wp-content"             "/wp-content/" "/wp-content/mu-plugins/"
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "wp-content/"            "/"            "/mu-plugins/"
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "wp-content/mu-plugins"  "/"            "/mu-plugins/"
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "wp-content/mu-plugins/" ""             "/"
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "wp-content/plugins"     ""             ""
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "wp-content/plugins/"    ""             ""
+    change_working_dir "tests/fixtures/src" run_test_determine_source_exclude_paths "."                      "/wp-content/" "/wp-content/mu-plugins/"
+
+    # Workdir -> wp-content directory
+    change_working_dir "tests/fixtures/src/wp-content" run_test_determine_source_exclude_paths "mu-plugins"  "/" "/mu-plugins/"
+    change_working_dir "tests/fixtures/src/wp-content" run_test_determine_source_exclude_paths "mu-plugins/" ""  "/"
+    change_working_dir "tests/fixtures/src/wp-content" run_test_determine_source_exclude_paths "plugins"     ""  ""
+    change_working_dir "tests/fixtures/src/wp-content" run_test_determine_source_exclude_paths "plugins/"    ""  ""
+    change_working_dir "tests/fixtures/src/wp-content" run_test_determine_source_exclude_paths "."           "/" "/mu-plugins/"
+  }
+
+  mount_from_wp_content() {
+    echo -e "${BLUE}INFO: mounting from /site/wp-content...${NC}"
+
+    # Mock the print_mount_paths function
+    print_mount_paths() {
+      # shellcheck disable=SC2317
+      printf "/path/to/site/wp-content /site"
+    }
+
+    # Workdir -> src directory
+    change_working_dir "tests/fixtures/src"            run_test_determine_source_exclude_paths "." "/" "/mu-plugins/"
+
+    # Workdir -> wp-content directory
+    change_working_dir "tests/fixtures/src/wp-content" run_test_determine_source_exclude_paths "." "/" "/mu-plugins/"
+  }
+
+  mount_from_mu_plugins() {
+    echo -e "${BLUE}INFO: mounting from /site/mu-plugins...${NC}"
+
+    # Mock the print_mount_paths function
+    print_mount_paths() {
+      # shellcheck disable=SC2317
+      printf "/path/to/site/wp-content/mu-plugins /site"
+    }
+
+    # Workdir -> src directory
+    change_working_dir "tests/fixtures/src"                       run_test_determine_source_exclude_paths "." "" "/"
+
+    # Workdir -> mu-plugins directory
+    change_working_dir "tests/fixtures/src/wp-content/mu-plugins" run_test_determine_source_exclude_paths "." "" "/"
+  }
+
+  mount_from_plugins() {
+    echo -e "${BLUE}INFO: mounting from /site/plugins...${NC}"
+
+    # Mock the print_mount_paths function
+    print_mount_paths() {
+      # shellcheck disable=SC2317
+      printf "/path/to/site/wp-content/plugins /site"
+    }
+
+    # Workdir -> src directory
+    change_working_dir "tests/fixtures/src"                    run_test_determine_source_exclude_paths "." "" ""
+
+    # Workdir -> plugins directory
+    change_working_dir "tests/fixtures/src/wp-content/plugins" run_test_determine_source_exclude_paths "." "" ""
+  }
+
+  mount_from_root
+  mount_from_wp_content
+  mount_from_mu_plugins
+  mount_from_plugins
+
+  echo -e "${GREEN}All tests passed for determining the source excludes path.${NC}"
+}
+
+test_determine_remote_exclude_paths() {
+  run_test_determine_remote_exclude_paths() {
+    export REMOTE_PATH=$1
+    local expected_base_path=$2
+    local expected_mu_dir_path=$3
+
+    {
+      IFS= read -r base_path || base_path=""
+      IFS= read -r mu_dir_path || mu_dir_path=""
+    } <<< "$(determine_remote_exclude_paths)"
+
+    if [[ "${base_path}" != "${expected_base_path}" || "${mu_dir_path}" != "${expected_mu_dir_path}" ]]; then
+      echo -e "${RED}Test failed for REMOTE_PATH='${SRC_PATH}': expected '$expected_base_path, $expected_mu_dir_path', got '$base_path, $mu_dir_path'.${NC}"
       exit 1
     fi
-  fi
+  }
 
-  # Fail if the output contains the excluded mu-plugin file
-  if echo "$output" | grep -q "mu-plugin.php"; then
-    echo -e "${RED}Test failed for pwd='$(pwd)', SRC_PATH='$SRC_PATH', REMOTE_PATH='$REMOTE_PATH': WPE mu-plugin.php included${NC}"
-    echo -e "${BLUE}$(log_debug "Rsync output:" "$output")\n${NC}"
-    echo -e "${BLUE}$(log_debug "Source exclude from:" "$source_exclude_from")\n${NC}"
-    echo -e "${BLUE}$(log_debug "Remote excludes:" "$remote_excludes")\n${NC}"
+  run_test_determine_remote_exclude_paths ""                       "/wp-content/" "/wp-content/mu-plugins/"
+  run_test_determine_remote_exclude_paths "wp-content"             "/"            "/mu-plugins/"
+  run_test_determine_remote_exclude_paths "wp-content/"            "/"            "/mu-plugins/"
+  run_test_determine_remote_exclude_paths "wp-content/mu-plugins"  ""             "/"
+  run_test_determine_remote_exclude_paths "wp-content/mu-plugins/" ""             "/"
+  run_test_determine_remote_exclude_paths "wp-content/plugins/"    ""             ""
+
+  echo -e "${GREEN}All tests passed for determining the remote excludes path.${NC}"
+}
+
+test_generate_source_exclude_from() {
+  export SRC_PATH="."
+  local output
+  local expected_output
+
+  # Mock the print_mount_paths function
+  print_mount_paths() {
+    # shellcheck disable=SC2317
+    printf "/path/to/site /site"
+  }
+
+  output=$(generate_source_exclude_from)
+  expected_output=$(cat "tests/fixtures/excludes/source_exclude_from.txt")
+
+  if [[ "$output" != "$expected_output" ]]; then
+    echo -e "${RED}Test failed: generated output does not match expected output.${NC}"
+    echo -e "${BLUE}Generated output:${NC}"
+    echo "$output"
+    echo -e "${BLUE}Expected output:${NC}"
+    echo "$expected_output"
     exit 1
   fi
+
+  echo -e "${GREEN}Test passed for source exclude from: generated output matches expected output.${NC}"
+}
+
+test_generate_remote_excludes() {
+  export REMOTE_PATH=""
+  local output
+  local expected_output
+
+  output=$(generate_remote_excludes)
+  expected_output=$(cat "tests/fixtures/excludes/remote_excludes.txt")
+
+  if [[ "$output" != "$expected_output" ]]; then
+    echo -e "${RED}Test failed: generated output does not match expected output.${NC}"
+    echo -e "${BLUE}Generated output:${NC}"
+    echo "$output"
+    echo -e "${BLUE}Expected output:${NC}"
+    echo "$expected_output"
+    exit 1
+  fi
+
+  echo -e "${GREEN}Test passed for source exclude from: generated output matches expected output.${NC}"
 }
 
 main() {
-  read -r mounted_src_path base_mount_path <<< "$(print_mount_paths "/site")"
-  echo -e "${BLUE}Info: mounted_src_path='$mounted_src_path', base_mount_path='$base_mount_path', pwd='$(pwd)'${NC}"
-
-  local test_cases=(
-    # SRC_PATH            REMOTE_PATH
-    "."                   ""
-    "."                   "wp-content"
-    "."                   "wp-content/" # Trailing slash in REMOTE_PATH
-    "."                   "wp-content/plugins"
-    "."                   "wp-content/plugins/" # Trailing slash in REMOTE_PATH
-    "."                   "wp-content/mu-plugins"
-    "."                   "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-  )
-
-  if [[ "$mounted_src_path" == *src && "$(pwd)" == *site ]]; then
-    test_cases+=(
-      "wp-content"          ""
-      "wp-content"          "wp-content"
-      "wp-content"          "wp-content/" # Trailing slash in REMOTE_PATH
-      "wp-content/"         ""
-      "wp-content/"         "wp-content"
-      "wp-content/"         "wp-content/" # Trailing slash in REMOTE_PATH
-      "wp-content/plugins"  ""
-      "wp-content/plugins"  "wp-content/plugins"
-      "wp-content/plugins"  "wp-content/plugins/" # Trailing slash in REMOTE_PATH
-      "wp-content/plugins/" "wp-content/plugins"
-      "wp-content/plugins/" "wp-content/plugins/" # Trailing slash in REMOTE_PATH
-      "wp-content/mu-plugins"  ""
-      "wp-content/mu-plugins"  "wp-content"
-#      "wp-content/mu-plugins"  "wp-content/mu-plugins"
-#      "wp-content/mu-plugins"  "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-#      "wp-content/mu-plugins/" "wp-content/mu-plugins"
-#      "wp-content/mu-plugins/" "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-#      "wp-content/mu-plugins"  ""
-#      "wp-content/mu-plugins"  "wp-content"
-#      "wp-content/mu-plugins"  "wp-content/mu-plugins"
-#      "wp-content/mu-plugins"  "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-#      "wp-content/mu-plugins/" "wp-content"
-#      "wp-content/mu-plugins/" "wp-content/mu-plugins"
-#      "wp-content/mu-plugins/" "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-    )
-  fi
-
-  if [[ "$mounted_src_path" == *wp-content && "$(pwd)" == *site ]]; then
-    test_cases+=(
-      "plugins"  ""
-      "plugins" "wp-content/plugins"
-      "plugins" "wp-content/plugins/" # Trailing slash in REMOTE_PATH
-      "plugins/" "wp-content/plugins"
-      "plugins/" "wp-content/plugins/" # Trailing slash in REMOTE_PATH
-      "mu-plugins"  ""
-#      "mu-plugins"  "wp-content/mu-plugins"
-#      "mu-plugins"  "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-#      "mu-plugins/" "wp-content/mu-plugins"
-#      "mu-plugins/" "wp-content/mu-plugins/" # Trailing slash in REMOTE_PATH
-    )
-  fi
-
-  for ((i=0; i<${#test_cases[@]}; i+=2)); do
-    echo -e "${BLUE}Generating rsync excludes for SRC_PATH='${test_cases[i]}', REMOTE_PATH='${test_cases[i+1]}'${NC}"
-    test_rsync_with_excludes "${test_cases[i]}" "${test_cases[i+1]}"
-  done
-
-  local flags=("--delete")
-  for ((i=0; i<${#test_cases[@]}; i+=2)); do
-    echo -e "${BLUE}Generating rsync excludes with flags='${flags[*]}' for SRC_PATH='${test_cases[i]}', REMOTE_PATH='${test_cases[i+1]}'${NC}"
-    test_rsync_with_excludes "${test_cases[i]}" "${test_cases[i+1]}" "${flags[@]}"
-  done
-
-  echo -e "${GREEN}Tests passed for mounted_src_path='$mounted_src_path', base_mount_path='$base_mount_path', pwd='$(pwd)'${NC}"
+  test_determine_source_exclude_paths
+  test_determine_remote_exclude_paths
+  test_generate_source_exclude_from
+  test_generate_remote_excludes
 }
 
 main
